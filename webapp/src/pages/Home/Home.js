d96f9e5e3962095a81cde48e51de97949df46ae5
Sanket Sawale <sawalesanket@gmail.com>
2022-09-24 04:20:28 +0530

    homepage update

--- webapp/src/pages/Home/Home.js
+++ webapp/src/pages/Home/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 import NavBar from "../../components/NavBar/NavBar.js"
 import Footer from "../../components/Footer/Footer.js"
 import Recommended from "../../components/Recommended/Recommended.js";
+import Landing from "../../components/Landing/Landing.js";
 
 function Home() {
   return (
     <>
       <NavBar />
+      <Landing />
       <Recommended/>
       <Footer />
     </>
