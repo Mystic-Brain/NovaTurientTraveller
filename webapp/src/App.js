b1501a770de5cdf9ef22b2eeb29293db7f4b5635
Sanket Sawale <sawalesanket@gmail.com>
2022-09-24 19:07:35 +0530

    homepage update

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import CampDetail from "./pages/CampDetail/CampDetail";
 import Contact from "./pages/Contact/Contact";
 import Home from "./pages/Home/Home";
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/campdetail" element={<CampDetail />} />
     </Routes>
     </>
   );
