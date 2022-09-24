3e05a05ef0f14c094ded303703e2358cff9ac506
Sanket Sawale <sawalesanket@gmail.com>
2022-09-24 16:12:54 +0530

    Home page and firebase change

--- webapp/src/pages/Home/Home.js
+++ webapp/src/pages/Home/Home.js
@@ -3,13 +3,15 @@ import NavBar from "../../components/NavBar/NavBar.js"
 import Footer from "../../components/Footer/Footer.js"
 import Recommended from "../../components/Recommended/Recommended.js";
 import Landing from "../../components/Landing/Landing.js";
+import Campings from "../../components/Campings/Campings.js";
 
 function Home() {
   return (
     <>
       <NavBar />
       <Landing />
-      <Recommended/>
+      <Recommended />
+      <Campings />
       <Footer />
     </>
   );
